89ed731f77cb0cf9668493b509d38bccfc663280
Yogeshwar <yogeshwar1710a@gmail.com>
2024-08-20 09:00:04 +0530

    Explicit type casting

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,5 +1,6 @@
 public class TemperatureConverter {
     public static void main(String args[]) {
+        // implecit type casting mean lower to higher data type
         int celsius = 25;
         double fahrenheit = (celsius * 9 / 5) + 32;
         System.out.println("Temperature in celsius: " + celsius);
