802362e612fb05a350b897b5d23afb714fa6aced
Yogeshwar <yogeshwar1710a@gmail.com>
2024-08-20 08:30:39 +0530

    Implicit type casting

---
+++ TemperatureConverter.java
@@ -0,0 +1,8 @@
+public class TemperatureConverter {
+    public static void main(String args[]) {
+        int celsius = 25;
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Temperature in celsius: " + celsius);
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+    }
+}
