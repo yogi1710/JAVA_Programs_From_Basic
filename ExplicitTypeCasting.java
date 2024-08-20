03964f986251ab098aa4d974bd9f539c44859f7e
Yogeshwar <yogeshwar1710a@gmail.com>
2024-08-20 09:26:11 +0530

    Explicit type casting

--- ExplicitTypeCasting.java
+++ ExplicitTypeCasting.java
@@ -1,7 +1,11 @@
 public class ExplicitTypeCasting {
     public static void main(String args[]) {
-        int x = 150;
-        byte b = (byte)x;
-        System.out.println(b);
+        // int x = 140;
+        // byte b = (byte) x;
+        // System.out.println(b);
+        double fahrenheit = 77.0;
+        int celsius = (int) ((fahrenheit - 32) * 5 / 9);
+        System.out.println("Temperature in fahreheit : " + fahrenheit);
+        System.out.println("Temperature in celsius : " + celsius);
     }
 }
