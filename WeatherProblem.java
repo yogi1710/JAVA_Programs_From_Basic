c97744b342da5400fed0bc59a472b9e4fd11b967
Yogeshwar <yogeshwar1710a@gmail.com>
2024-08-26 08:34:52 +0530

    Conditional Statements with weather example

---
+++ WeatherProblem.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class WeatherProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the temperature in Celsius: ");
+        double celsius = sc.nextDouble();
+        if (celsius > 40) {
+            System.out.println("It's too hot, I am a cotton cloth.");
+        } else if (celsius > 20) {
+            System.out.println("I am a cotton cloth, but I am not too hot.");
+        } else if (celsius > 10 && celsius < 20) {
+            System.out.println("I am a bit cold.");
+        } else if (celsius <=10 ){
+            System.out.println("I am too cold, I am a woolen cloth.");
+        }
+        sc.close();
+    }
+}
