762a2c047c18bd9efab3fc6e250c9217b9d917aa
Yogeshwar <yogeshwar1710a@gmail.com>
2024-08-27 09:34:34 +0530

    find all roots of a quadratic equation

---
+++ quadraticEquation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+// 17. Write a program to find all roots of a quadratic equation
+
+class quadraticEquation {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the coefficients of the quadratic equation");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double determinant = b * b - 4 * a * c;
+        double root1, root2;
+        if (determinant > 0) {
+            root1 = (-b + Math.sqrt(determinant)) / 2 * a;
+            root2 = (-b - Math.sqrt(determinant)) / 2 * a;
+            System.out.format("The roots of the quadratic equation are %.2f and %.2f", root1, root2);
+        } else if (determinant == 0) {
+            root1 = root2 = -b / 2 * a;
+            System.out.format("The roots of the quadratic equation are %.2f and %.2f", root1, root2);
+        } else {
+            double real = -b / (2 * a);
+            double img = Math.sqrt(-determinant) / (2 * a);
+            System.out.format("root1 = %.2f+%.2fi", real, img);
+            System.out.format("\nroot2 = %.2f-%.2fi", real, img);
+        }
+        sc.close();
+    }
+}
