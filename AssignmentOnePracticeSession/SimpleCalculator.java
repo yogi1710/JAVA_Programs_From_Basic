747068bf4f1048b822ef24b886a4599d20ec6852
Yogeshwar <yogeshwar1710a@gmail.com>
2024-08-28 10:54:07 +0530

    Calculator with simple operations between 2 numbers

--- AssignmentOnePracticeSession/SimpleCalculator.java
+++ AssignmentOnePracticeSession/SimpleCalculator.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 
 class SimpleCalculator {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number : ");
         double num1 = sc.nextDouble();
         System.out.println("Enter the second number : ");
         double num2 = sc.nextDouble();
-        System.out.println("Enter the operator (+, -, *, /) : ");
+        System.out.println("Enter the operator (+, -, *, /, %) : ");
         char op = sc.next().charAt(0);
         double result = 0;
         switch (op) {
             case '+':
-            result = num1 + num2;
-            break;
+                result = num1 + num2;
+                break;
             case '-':
-            result = num1 - num2;
-            break;
+                result = num1 - num2;
+                break;
             case '*':
-            result = num1 * num2;
-            break;
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
             case '%':
-            result = num1 % num2;
-            break;
+                result = num1 % num2;
+                break;
         }
-        System.out.println("The result is : "+ result);
+        System.out.println("The result is : " + result);
     }
 }
