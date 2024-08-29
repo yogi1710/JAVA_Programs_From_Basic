9bfb992792c8f3f35c46b5e385fdf1e244171c4f
Yogeshwar <yogeshwar1710a@gmail.com>
2024-08-30 00:39:19 +0530

    print all even numbers between 1 to 100

---
+++ LoopingProblems/PrintingEvenNumber.java
@@ -0,0 +1,9 @@
+// Write a program to print all even numbers between 1 to 100
+
+class PrintingEvenNumber {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i = i + 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
