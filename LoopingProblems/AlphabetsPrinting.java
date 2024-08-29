296ddc5d32a9a9afc359d9bd9508a9c9fc6aadd6
Yogeshwar <yogeshwar1710a@gmail.com>
2024-08-30 00:25:06 +0530

    print all alphabets from a to z

---
+++ LoopingProblems/AlphabetsPrinting.java
@@ -0,0 +1,12 @@
+// Write a program to print all alphabets from a to z
+
+import java.util.Scanner;
+
+class AlphabetsPrinting {
+    public static void main(String[] args) {
+        System.out.println("All alphabets from a to z : ");
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
