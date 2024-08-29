73107d4f542cb288cfaffa9b76f2bd03d41d70e0
Yogeshwar <yogeshwar1710a@gmail.com>
2024-08-30 00:43:36 +0530

    print all odd number between 1 to 100

---
+++ LoopingProblems/PrintingOddNumber.java
@@ -0,0 +1,9 @@
+// Write a program to print all odd number between 1 to 100
+
+class PrintingOddNumber {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i = i + 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
