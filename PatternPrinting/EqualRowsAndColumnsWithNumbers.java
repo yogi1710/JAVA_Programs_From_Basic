dc3ffe914562abdf46dc85c2cee9436548becbfb
Yogeshwar <yogeshwar1710a@gmail.com>
2024-09-02 09:17:40 +0530

    Right angle triangle pattern printing

--- PatternPrinting/EqualRowsAndColumnsWithNumbers.java
+++ PatternPrinting/EqualRowsAndColumnsWithNumbers.java
@@ -12,13 +12,18 @@ public class EqualRowsAndColumnsWithNumbers {
             for (int j = 0; j < n; j++) {
                 System.out.print(j + " ");
                 // System.out.print ((char)(97+j)+" ");
-                // To print the alphabets we need to typecase the j value by adding 97 to j value
-                // for every iteration, we will add 97 that is asscii value of 'a', hance it will convert the vlaue into the alphabetical form
+                // To print the alphabets we need to typecase the j value by adding 97 to j
+                // value
+                // for every iteration, we will add 97 that is asscii value of 'a', hence it
+                // will convert the vlaue into the alphabetical form
                 // For capital values we use ascci value of 'A' that is 65
 
-                // to print the alphabets in the reverse order that is form 'z', we use the ascci value of the 'z' that is 122
-                // similarly we can start the values from any alphabets by using the assci values, the only thing we need to do is, we should be aware of assci values and should perform the type casting.
-                // System.out.print((n-1-j)+" "); 
+                // to print the alphabets in the reverse order that is form 'z', we use the
+                // ascci value of the 'z' that is 122
+                // similarly we can start the values from any alphabets by using the assci
+                // values, the only thing we need to do is, we should be aware of assci values
+                // and should perform the type casting.
+                // System.out.print((n-1-j)+" ");
                 // This will print the values from n-1 to 0
                 // withput changing the loop values we can do this, short method
             }
---
+++ PatternPrinting/RightAngleTrianglePrinting.java
@@ -0,0 +1,17 @@
+// package PatternPrinting;
+
+import java.util.Scanner;
+
+public class RightAngleTrianglePrinting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n value : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
