66f8f44d6758a56d9e26e0bb33cee8c66ca951a0
Yogeshwar <yogeshwar1710a@gmail.com>
2024-09-02 08:36:06 +0530

    Printing same rows and columns by taking input from user

--- PatternPrinting/EqualRowsAndColumns.java
+++ PatternPrinting/EqualRowsAndColumns.java
@@ -10,7 +10,7 @@ public class EqualRowsAndColumns {
         sc.close();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                System.out.print(j + " ");
+                System.out.print("* ");
             }
             System.out.println();
         }
