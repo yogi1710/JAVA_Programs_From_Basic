bef0c8ed20ed018e612e59fd2cc7bb95544249bc
Yogeshwar <yogeshwar1710a@gmail.com>
2024-09-03 08:34:01 +0530

    Printing the reverse right angle triangle with stars *

---
+++ PatternPrinting/ReverseRightAngleTriangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ReverseRightAngleTriangle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row value : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
